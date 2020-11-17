fb106ea5d1d035cf1068ff4c990111147c80bda2
chusloj <pyrotekk66@gmail.com>
2020-11-16 22:22:41 -0500

    updated vending.py

--- vending_machine/vending.py
+++ vending_machine/vending.py
@@ -1,18 +1,19 @@
 import random
+import os
 
-# make a 3-level vending machine with 15 items
 def make_prices(num, price_range):
-    """ Create a list of prices """
 
-    prices = [random.randrange(price_range[0], price_range[1]) * 0.25 for _ in range(num)]
+    """ Create a list of prices """
+    prices = [random.randrange(
+        price_range[0], price_range[1]) * 0.25 for _ in range(num)]
     return prices
 
-
 def make_items(price_list):
+
     """ Create a list of items """
 
     items = []
-    letters = ['A','B','C','D','E']
+    letters = ['A', 'B', 'C', 'D', 'E']
     for n in range(len(price_list)):
         l_count = n // 5
         items.append(f'{letters[l_count]}{(n%5)+1}')
@@ -20,7 +21,9 @@ def make_items(price_list):
 
 
 
+
 def make_vending_machine(num, price_range):
+
     """ Create a vending machine """
 
     if num > 25:
@@ -34,7 +37,11 @@ def make_vending_machine(num, price_range):
 
     return items, prices
 
+
+
+
 def make_descriptions(items):
+
     """ Create descriptions for the items in the vending machine """
 
     l = len(items)
@@ -43,8 +50,8 @@ def make_descriptions(items):
     return food_list
 
 
+def print_vending_machine(items, prices):  # keep text formatting here
 
-def print_vending_machine(items, prices): # keep text formatting here
     """ 
     Format the vending machine
     to fit nicely inside the command prompt
@@ -52,17 +59,16 @@ def print_vending_machine(items, prices): # keep text formatting here
     """
 
     for n in range(len(prices)):
-        if (n+1) % 5 == 0:
-            item_line = [items[j] for j in range(n-4, n+1)]
-            price_line = [f"${str(prices[k])}" for k in range(n-4,n+1)]
-
+        if (n + 1) % 5 == 0:
+            item_line = [items[j] for j in range(n - 4, n + 1)]
+            price_line = [f"${str(prices[k])}" for k in range(n - 4, n + 1)]
 
             for n, (p, i) in enumerate(zip(price_line, item_line)):
                 if i != 'SOLD OUT':
                     len_diff = len(p) - len(i)
                     item_line[n] += (' ' * len_diff)
                 else:
-                    len_diff = 8 - len(p)    
+                    len_diff = 8 - len(p)
                     price_line[n] += (' ' * len_diff)
 
             print(' || '.join(item_line))
@@ -71,14 +77,16 @@ def print_vending_machine(items, prices): # keep text formatting here
 
 
 
+
 def runner():
+
     """ Defines the interface to the vending machine """
 
     print("How many entries would you like in your vending machine?",
-          "Please keep the number of entries <= 25.")
+    "Please keep the number of entries <= 25.")
     num = int(input())
 
-    items, prices = make_vending_machine(num, [1,8])
+    items, prices = make_vending_machine(num, [1, 8])
     food_list = make_descriptions(items)
 
     print("How much money do you have?")
@@ -87,6 +95,7 @@ def runner():
     sold_out_list = []
     while (any(prices) != 'SOLD OUT') or (money <= 0):
         if len(sold_out_list) == len(items):
+            os.system('clear')
             print("This machine is empty!\n")
             print(f"You have ${money} remaining.\n")
             return
@@ -98,7 +107,9 @@ def runner():
         print('\n')
         print("What's your selection?")
 
-        choice = input() # input is type: string by default
+        choice = input()  # input is type: string by default
+
+        os.system('clear')
 
         if choice in sold_out_list:
             print("This item is sold out! Please choose another item.")
@@ -118,10 +129,6 @@ def runner():
 
         # This lien has to be placed after all other lines
         items[items.index(choice)] = 'SOLD OUT'
-
-        
-
-
 
 
 # Driver
