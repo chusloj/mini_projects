57bcb365b14ab6e58994a4741f92c7658382c472
chusloj <pyrotekk66@gmail.com>
2020-12-20 22:02:24 -0500

    updated board logic to oop

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -1,22 +1,28 @@
 import curses
-from curses import wrapper
+# from curses import wrapper
 
 class Board():
+
     def __init__(self, blen, bwidth):
+
         self.blen = blen
         self.bwidth = bwidth
-        self.piece_col = 3
-        self.piece_row = 0
+        
+        # board representation
+        self.env_map = [[' ' for _ in range(bwidth)] for _ in range(blen)]
+        self.piece_loc = []
 
+        # Initialization settings
         self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
         curses.curs_set(0)
         self.stdscr.keypad(True)
 
+        # clear screen
         self.stdscr.clear()
 
-    def Draw_Board(self):
+        # Draw board
         for i in range(0, self.blen+1):
             self.stdscr.addstr(i, 0, '#')
             self.stdscr.addstr(i, self.bwidth, '#')
@@ -24,72 +30,98 @@ class Board():
         for i in range(0, self.bwidth+1):
             self.stdscr.addstr(self.blen, i, '#')
 
-    def Move_Piece(self, c):
-        if c == curses.KEY_DOWN:
-            if self.piece_row == (self.blen - 1):
-                return
-            for i in range(self.piece_col, self.piece_col + 4):
-                self.stdscr.addstr(self.piece_row, i, ' ')
-                self.stdscr.addstr(self.piece_row+1, i, '#')
-            self.piece_row += 1
-        elif c == curses.KEY_UP:
-            if self.piece_row == 0:
-                return
-            for i in range(self.piece_col, self.piece_col + 4):
-                self.stdscr.addstr(self.piece_row, i, ' ')
-                self.stdscr.addstr(self.piece_row-1, i, '#')
-            self.piece_row -= 1
-        elif c == curses.KEY_LEFT:
-            if (self.piece_col - 1) == 0:
-                return
-            for i in range(self.piece_col, self.piece_col + 4):
-                self.stdscr.addstr(self.piece_row, i, ' ')
-            for i in range(self.piece_col - 1, self.piece_col + 3):
-                self.stdscr.addstr(self.piece_row, i, '#')
-            self.piece_col -= 1
-        elif c == curses.KEY_RIGHT:
-            if (self.piece_col + 4) == self.bwidth:
-                return
-            for i in range(self.piece_col, self.piece_col + 4):
-                self.stdscr.addstr(self.piece_row, i, ' ')
-            for i in range(self.piece_col + 1, self.piece_col + 5):
-                self.stdscr.addstr(self.piece_row, i, '#')
-            self.piece_col += 1
-        elif c == ord('r'):
-            for i in range(self.piece_col, self.piece_col + 4):
-                self.stdscr.addstr(self.piece_row, i, ' ')
-            for i in range(self.piece_row, self.piece_row + 4):
-                self.stdscr.addstr(i, self.piece_col, '#')
+        # test piece
+        for i in range(3,7):
+            self.stdscr.addstr(0, i, '#')
+            self.piece_loc.append( [0, i] )
 
-    def Rotate_Piece(self):
-        for i in range(self.piece_col, self.piece_col + 4):
-            self.stdscr.addstr(self.piece_row, i, ' ')
-        for i in range(self.piece_row, self.piece_row + 4):
-            self.stdscr.addstr(i, self.piece_col, '#')
+        self.stdscr.refresh()
+
+
+    def Move_Piece(self, k):
+
+        def Write(row, col, piece):
+            self.stdscr.addstr(row, col, piece)
+
+        def Erase(row, col):
+            self.stdscr.addstr(row, col, ' ')
+
+        if k == curses.KEY_DOWN:
+            if max([i[0] for i in self.piece_loc]) == (self.blen - 1):
+                return
+
+            for i, j in self.piece_loc:
+                Erase(i, j)
+                Write(i+1, j, '#')
+
+            for n, (i, j) in enumerate(self.piece_loc):
+                self.piece_loc[n][0] = i+1
+
+        elif k == curses.KEY_UP:
+            if min([i[0] for i in self.piece_loc]) == 0:
+                return
+
+            for i, j in self.piece_loc:
+                Erase(i, j)
+                Write(i-1, j, '#')
+
+            for n, (i, j) in enumerate(self.piece_loc):
+                self.piece_loc[n][0] = i-1
+
+        elif k == curses.KEY_LEFT:
+            if min([i[1] for i in self.piece_loc]) == 1:
+                return
+
+            for i, j in self.piece_loc:
+                Erase(i, j)
+            for i, j in self.piece_loc:
+                Write(i, j-1, '#')
+
+            for n, (i, j) in enumerate(self.piece_loc):
+                self.piece_loc[n][1] = j-1
+
+        elif k == curses.KEY_RIGHT:
+            if max([i[1] for i in self.piece_loc]) == (self.bwidth - 1):
+                return
+
+            for i, j in self.piece_loc:
+                Erase(i, j)
+            for i, j in self.piece_loc:
+                Write(i, j+1, '#')
+
+            for n, (i, j) in enumerate(self.piece_loc):
+                self.piece_loc[n][1] = j+1
+
+        elif k == ord('r'):
+            for i, j in self.piece_loc:
+                Erase(i, j)
+
+            for n, (i, j) in enumerate(self.piece_loc):
+                self.piece_loc[n] = [j, i]
+
+            for i, j in self.piece_loc:
+                Write(i, j, '#')
+
+
+
+
 
 
 
 def main():
-    # Initialize
+
     b = Board(12, 18)
 
-    b.Draw_Board()
-
-    # # draw tetromino
-    for i in range(3,7):
-        b.stdscr.addstr(0, i, '#')
-
-    c = b.stdscr.getch()
-    while c != ord('q'):
-        b.Move_Piece(c)
+    k = b.stdscr.getch()
+    while k != ord('q'):
+        b.Move_Piece(k)
         b.stdscr.refresh()
-        c = b.stdscr.getch()
+        k = b.stdscr.getch()
 
     # # end program
     curses.nocbreak()
     b.stdscr.keypad(False)
     curses.echo()
     curses.endwin()
-
 
 main()
