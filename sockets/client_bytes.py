d1a1668ebc53f5cce27a24ee8fc9c8b9647a4f0e
chusloj <chusloj@gmail.com>
2022-08-16 23:47:54 -0400

    formatting using black

--- sockets/client_bytes.py
+++ sockets/client_bytes.py
@@ -1,28 +1,19 @@
 import socket
-
-"""
-Build a client using a TCP socket with a
-target host IP address and a target host port.
-"""
+import pickle
 
 HEADERSIZE = 10
 
-HOST = "192.168.4.29"  # Host of the server (Target host)
-PORT = 9090  # Target port on server
+HOST = "localhost"  # Host of the server (Target host)
+PORT = 9099  # Target port on server
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((HOST, PORT))
 
 socket.send("Hello World!".encode("utf-8"))
 
-# msg = socket.recv(8)
-# while msg:
-#     print(msg.decode('utf-8'))
-#     msg = socket.recv(8)
-
 byte_size = 16  # or 1024
 
-full_msg = ""
+full_msg = b""
 new_msg = True
 while True:
     msg = socket.recv(byte_size)
@@ -34,15 +25,20 @@ while True:
         print(f"new message length: {msglen}")
         new_msg = False
 
-    full_msg += msg.decode("utf-8")
+    full_msg += msg
 
     if (
         len(full_msg) - HEADERSIZE == msglen
     ):  # if you've read in the length of the message body + length of the header, that's the whole message
         print("Full message received")
-        print(full_msg[:HEADERSIZE])  # header
-        print(full_msg[HEADERSIZE:])  # message body
+        print(
+            full_msg[:HEADERSIZE].decode("utf-8")
+        )  # header, decoding now happens here
+
+        d = pickle.loads(full_msg[HEADERSIZE:])  # message body (pickled)
+        print(d)
+
         new_msg = True
-        full_msg = ""
+        full_msg = b""
 
         print("\n")
--- sockets/server.py
+++ sockets/server.py
@@ -9,25 +9,25 @@ Build a server using a TCP socket.
 HEADERSIZE = 10
 
 
-HOST = "192.168.4.29" # Private local IP address
-PORT = 9090 # you can choose any port
+HOST = "192.168.4.29"  # Private local IP address
+PORT = 9090  # you can choose any port
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
-server.listen(5) # accept only 5 connections, refuse the rest
+server.listen(5)  # accept only 5 connections, refuse the rest
 
 while True:
     communication_socket, address = server.accept()
     # Note: communication_socket is NOT the same as server
     print(f"Connected to {address}")
-    
-    message = communication_socket.recv(1024) # argument is buffer size
+
+    message = communication_socket.recv(1024)  # argument is buffer size
     print(f"Message from client: {message.decode('utf-8')}")
 
     message_send = "Message received!"
     message_send = f"{len(message_send):<{HEADERSIZE}}" + message_send
-    communication_socket.send(message_send.encode('utf-8'))
+    communication_socket.send(message_send.encode("utf-8"))
 
     time.sleep(3)
 
@@ -35,11 +35,12 @@ while True:
     while new_msg_counter <= 10:
         message_send = f"This is new message {new_msg_counter}"
         message_send = f"{len(message_send):<{HEADERSIZE}}" + message_send
-        communication_socket.send(message_send.encode('utf-8'))
+        communication_socket.send(message_send.encode("utf-8"))
         new_msg_counter += 1
         time.sleep(1)
-    
+
     print("All done.")
     communication_socket.close()
     print(f"Connection with {address} ended.")
-    break
+
+    break  # end server program
